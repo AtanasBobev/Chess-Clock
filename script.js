963485f0951b8165d4da7a6cf8d9df6f6718cae6
AtanasBobev <a.bobev23@acsbg.org>
2020-06-22 09:20:09 +0300

    Additional menu items added

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ document.querySelector("#main").classList.remove('d-none');
     document.querySelector("#main").classList.add('d-none');
   }
 }
+let setupGame={
+  dataEntry:function(minutes,seconds,additionalSeconds){
+    display.hideMenu();
+    display.showSelection();
+  document.querySelector("#minutesPlayer1").value=minutes;
+  document.querySelector("#minutesPlayer2").value=minutes;
+  document.querySelector("#secondsPlayer1").value=seconds;
+  document.querySelector("#secondsPlayer2").value=seconds;
+document.querySelector("#additionalSeconds1").value=additionalSeconds;
+document.querySelector("#additionalSeconds2").value=additionalSeconds;
+  }
+}

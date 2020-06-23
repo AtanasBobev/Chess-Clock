8a3a35aee8cd410823bf0642f326d4122ca1d500
AtanasBobev <a.bobev23@acsbg.org>
2020-06-23 09:27:43 +0300

    -Win and lose system added -Fixed some bugs releating the change timer function -Added pause menu

--- script.js
+++ script.js
@@ -36,10 +36,12 @@ let setupGame = {
             display.hideSelection();
             display.showDesktopClock();
             inGame.initialEstablish();
-            this.TimePlayer1 = ((document.querySelector("#minutesPlayer1").value * 60) + document.querySelector("#secondsPlayer1").value) / 10;
-            this.TimePlayer2 = ((document.querySelector("#minutesPlayer2").value * 60) + document.querySelector("#secondsPlayer2").value) / 10;
-            document.querySelector("#secondClock").innerText = inGame.secondsToMinutes(this.TimePlayer2)
-            document.querySelector("#firstClock").innerText = inGame.secondsToMinutes(this.TimePlayer1)
+            this.TimePlayer1 = ((document.querySelector("#minutesPlayer1").value * 60) + Number(document.querySelector("#secondsPlayer1").value));
+            this.TimePlayer2 = ((document.querySelector("#minutesPlayer2").value * 60) + Number(document.querySelector("#secondsPlayer2").value));
+            document.querySelector("#secondClock").innerText = inGame.secondsToMinutes(this.TimePlayer2);
+            document.querySelector("#firstClock").innerText = inGame.secondsToMinutes(this.TimePlayer1);
+            document.querySelector("#name1Game").innerText=document.querySelector("#name1").value;
+            document.querySelector("#name2Game").innerText=document.querySelector("#name2").value;
 
 
             Swal.fire({
@@ -56,8 +58,8 @@ let setupGame = {
                     inGame.initialEstablish();
                     inGame.startTimer();
                 } else {
-display.hideDesktopClock();
-display.showSelection();
+                    display.hideDesktopClock();
+                    display.showSelection();
                 }
             })
 
@@ -130,62 +132,180 @@ let inGame = {
     },
     startTimer: function() {
         Player1Interval = setInterval(this.decreaseTimePlayer1, 1000);
-        document.querySelector("#firstClock").style.textDecoration='underline';
+        document.querySelector("#firstClock").style.textDecoration = 'underline';
+        document.querySelector("#name1Game").style.color = 'black';
+        document.querySelector("#name2Game").style.color = 'grey';
+
     },
     decreaseTimePlayer1: function() {
         inGame.timeLeftPlayer1--;
         if (inGame.timeLeftPlayer1 <= 0) {
             clearInterval(Player1Interval);
             document.querySelector("#firstClock").innerText = "No time left!";
-            document.querySelector("#firstClock").style.fontSize='5vmax';
-            document.querySelector("#firstClock").style.color='red';
-            inGame.game=false;
+            document.querySelector("#firstClock").style.fontSize = '5vmax';
+            document.querySelector("#firstClock").style.color = 'red';
+            document.querySelector("#firstClock").style.textDecoration = 'none';
+            document.querySelector("#firstClock").style.border = '1px solid red';
+            document.querySelector("#firstClock").style.padding = '1vmax';
+            document.querySelector("#firstClock").style.textAlign = 'center';
+inGame.endGameToLobby(1)
+            inGame.game = false;
         } else {
             document.querySelector("#firstClock").innerText = inGame.secondsToMinutes(Number(inGame.timeLeftPlayer1));
         }
     },
-    Player1IntervalRunning:true,
-    Player2IntervalRunning:false,
-    game:true,
+    Player1IntervalRunning: true,
+    game: true,
     decreaseTimePlayer2: function() {
         inGame.timeLeftPlayer2--;
         if (inGame.timeLeftPlayer2 <= 0) {
-              clearInterval(Player2Interval);
+            clearInterval(Player2Interval);
             document.querySelector("#secondClock").innerText = "No time left!";
-            document.querySelector("#secondClock").style.fontSize='5vmax';
-            document.querySelector("#secondClock").style.color='red';
-            inGame.game=false;
+            document.querySelector("#secondClock").style.fontSize = '5vmax';
+            document.querySelector("#secondClock").style.color = 'red';
+            document.querySelector("#secondClock").style.textDecoration = 'none';
+            document.querySelector("#secondClock").style.border = '1px solid red';
+            document.querySelector("#secondClock").style.padding = '1vmax';
+            document.querySelector("#secondClock").style.textAlign = 'center';
+            inGame.endGameToLobby(2)
+            inGame.game = false;
         } else {
             document.querySelector("#secondClock").innerText = inGame.secondsToMinutes(Number(inGame.timeLeftPlayer2));
         }
     },
 
-    switchRole:function(){
-      if(inGame.game){
-      if(inGame.Player1IntervalRunning){
-      clearInterval(Player1Interval);
-      Player2Interval = setInterval(inGame.decreaseTimePlayer2, 1000);
-      inGame.Player1IntervalRunning=false;
-      inGame.Player2IntervalRunning=true;
-      document.querySelector("#firstClock").style.textDecoration='none';
-      document.querySelector("#secondClock").style.textDecoration='underline';
-    }else{
-      document.querySelector("#firstClock").style.textDecoration='underline';
-      document.querySelector("#secondClock").style.textDecoration='none';
-      clearInterval(Player2Interval);
-      Player1Interval = setInterval(inGame.decreaseTimePlayer1, 1000);
-      inGame.Player2IntervalRunning=false;
-      inGame.Player1IntervalRunning=true;
-       }
-    }else{
-      clearInterval(Player1Interval);
-      clearInterval(Player2Interval);
-    }
-  }
+    switchRole: function() {
+        if (inGame.game) {
+            if (inGame.Player1IntervalRunning) {
+                clearInterval(Player1Interval);
+                Player2Interval = setInterval(inGame.decreaseTimePlayer2, 1000);
+                inGame.Player1IntervalRunning = false;
+                document.querySelector("#firstClock").style.textDecoration = 'none';
+                document.querySelector("#secondClock").style.textDecoration = 'underline';
+                document.querySelector("#name1Game").style.color = 'grey';
+                document.querySelector("#name2Game").style.color = 'black';
+            } else {
+              document.querySelector("#name1Game").style.color = 'black';
+              document.querySelector("#name2Game").style.color = 'grey';
+                document.querySelector("#firstClock").style.textDecoration = 'underline';
+                document.querySelector("#secondClock").style.textDecoration = 'none';
+                clearInterval(Player2Interval);
+                Player1Interval = setInterval(inGame.decreaseTimePlayer1, 1000);
+                inGame.Player1IntervalRunning = true;
+            }
+        } else {
+            clearInterval(Player1Interval);
+            clearInterval(Player2Interval);
+        }
+      },
+        pausedState:false,
+            pauseGame: function() {
+                if (!this.pausedState) {
+                    if (inGame.Player1IntervalRunning) {
+                        clearInterval(Player1Interval);
+                    } else {
+                        clearInterval(Player2Interval);
+                    }
+
+                this.pausedState = true;
+                document.querySelector("#pause").src='Images/Play.svg'
+            } else {
+              this.pausedState = false;
+                if (inGame.Player1IntervalRunning) {
+                    Player1Interval = setInterval(inGame.decreaseTimePlayer1, 1000);
+                } else {
+                    Player2Interval = setInterval(inGame.decreaseTimePlayer2, 1000);
+                }
+                document.querySelector("#pause").src='Images/Pause.svg';
+            }
+          },
+          endGameToLobby:function(lost){
+            if(lost==1){
+            anime({
+              targets: '#name1Game',
+              transform:'scale(0.5,0.5)',
+              left: '-100%',
+              easing: 'easeInOutQuad',
+            });
+            anime({
+              targets: '#firstClock',
+              transform:'scale(0.5,0.5)',
+              left: '-100%',
+              easing: 'easeInOutQuad'
+            });
+            anime({
+              targets: '#pause',
+              transform:'scale(0.5,0.5)',
+              left: '-100%',
+              easing: 'easeInOutQuad'
+            });
+            anime({
+              targets: '#name2Game',
+              deley:2000,
+              left: '50%',
+              top:"40%",
+              easing: 'easeInOutQuad'
+            });
+            anime({
+            targets: '#secondClock',
+            deley:3000,
+            left: '50%',
+            top:"80%",
+            easing: 'easeInOutQuad'
+          });
+          document.querySelector("#name2Game").style.color='gold';
+          document.querySelector("#name2Game").style.fontSize='8vmax';
+          document.querySelector("#name2Game").innerText+=" won";
+          document.querySelector("#secondClock").style.fontSize='10vmax'
+          document.querySelector("#secondClock").style.color='gold'
+          document.querySelector("#name2Game").style.textDecoration='underline'
+
+        }else{
+          anime({
+            targets: '#name2Game',
+            left: '-100%',
+            easing: 'easeInOutQuad',
+          });
+          anime({
+            targets: '#secondClock',
+            left: '-100%',
+            easing: 'easeInOutQuad'
+          });
+          anime({
+            targets: '#pause',
+            transform:'scale(0.5,0.5)',
+            left: '-100%',
+            easing: 'easeInOutQuad'
+          });
+          anime({
+            targets: '#name1Game',
+            deley:2000,
+            left: '50%',
+            top:"40%",
+            easing: 'easeInOutQuad'
+          });
+          anime({
+          targets: '#firstClock',
+          deley:3000,
+          left: '50%',
+          top:"80%",
+          easing: 'easeInOutQuad'
+        });
+        document.querySelector("#name1Game").style.color='gold';
+        document.querySelector("#name1Game").style.fontSize='8vmax';
+        document.querySelector("#name1Game").innerText+=" won";
+        document.querySelector("#firstClock").style.fontSize='10vmax'
+        document.querySelector("#firstClock").style.color='gold'
+        document.querySelector("#name1Game").style.textDecoration='underline'
+        }
+        }
+
+
 }
-document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-inGame.switchRole();
+document.body.onkeyup = function(e) {
+  if (e.keyCode == 32 && !inGame.pausedState) {
+        inGame.switchRole();
     }
 }
-let Player1Interval=0, Player2Interval=0;
+let Player1Interval = 0,
+    Player2Interval = 0;
